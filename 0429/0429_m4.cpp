bd6686ee9b4e9d31c59cc61fd56992dd7d500885
JungInYeong <167291569+JungInYeong@users.noreply.github.com>
2024-04-29 20:22:04 +0900

    dd

---
+++ 0429/0429_m4.cpp
@@ -0,0 +1,54 @@
+//실습1 list사용해보기
+
+#include <iostream>
+#include <list>
+#include <algorithm>
+using namespace std;
+
+void printmy(list<int> myList)
+{
+	for (list<int>::iterator iter = myList.begin(); iter != myList.end(); iter++)
+	{
+		cout << *iter << " ";
+	}
+}
+
+int main()
+{
+	list<int> myList = { 5,4,3,4,2,1,1 };
+
+	int count = 0;
+	for (list<int>::iterator iter = myList.begin(); iter != myList.end(); iter++)
+	{
+		if (*iter == 4)
+		{
+			count++;
+		}
+	}
+	cout << "4는 " << count << "개 입니다." << endl;
+
+	cout << endl;
+	
+	myList.sort();
+	printmy(myList);
+
+	cout << endl;
+
+	myList.unique();
+	printmy(myList);
+
+	cout << endl;
+
+	list<int>List = { 6,7 };
+	myList.splice(myList.end(), List);
+	printmy(myList);
+
+	cout << endl;
+
+	list<int>List2 = { 0 };
+	myList.splice(myList.end(), List2);
+	printmy(myList);
+
+
+	return 0;
+}
